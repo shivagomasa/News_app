095e946a1c8fd94c711cd1e5408f52b6184eaf98
shivagomasa <shivagomasa@gmail.com>
2019-03-16 09:41:36 +0530

    Create a post to admin interface.

--- Newsapp/models.py
+++ Newsapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    class Meta:
+        verbose_name_plural ='Categories'
+
+    category = models.CharField(max_length=79)
+
+    def __str__(self):
+        return self.category
+
